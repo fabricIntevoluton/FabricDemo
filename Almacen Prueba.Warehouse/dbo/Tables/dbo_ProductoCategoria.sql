2ebe764cb5266fbfe28c454727bdd90e1ea029a8
fabricIntevoluton <fabric@intevolution.ai>
2024-07-23 13:29:06 -0700

    Committing 3 items from workspace 1a773e32-1bcb-407e-86d8-76b25bea885e

--- Almacen Prueba.Warehouse/dbo/Tables/dbo_ProductoCategoria.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[dbo_ProductoCategoria] (
-
-	[ProductoCategoriaClave] int NULL, 
-	[ProductCategoryAlternateKey] int NULL, 
-	[NombreDeCategoriaDeProductoEnIngles] varchar(8000) NULL, 
-	[NombreDeCategoriaDeProductoEnEspanol] varchar(8000) NULL, 
-	[NombreDeCategoriaDeProductoEnFrances] varchar(8000) NULL
-);
-
--- Almacen Prueba.Warehouse/dbo/Tables/dbo_Territorio.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[dbo_Territorio] (
-
-	[TerritorioClave] int NULL, 
-	[Region] varchar(8000) NULL, 
-	[Pais] varchar(8000) NULL, 
-	[Grupo] varchar(8000) NULL
-);
-
